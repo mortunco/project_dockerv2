0f84646835f80b3960e91b41c7cfebcd83cd6918
mortunco <morovatunc@gmail.com>
2018-12-04 17:32:20 +0300

    Update firestarter.sh
    changed relative path problem on firestarter.sh

--- LL_pipeline/project/Batch1/firestarter.sh
+++ LL_pipeline/project/Batch1/firestarter.sh
@@ -1,13 +1,21 @@
+echo "Please run the script with"
+echo "the absolute path to project_dockerv2/LL_pipeline/project directory"
+echo "for example, if this is located at your home directory than it should be"
+echo "/home/favorite_user_name/some/directories/project_dockerv2/LL_pipeline/"
+echo "Script will add this prefix to the relative paths"
+echo "to input run it like sh firestarter.sh /home/favorite_user_name/some/directories/project_dockerv2/LL_pipeline/"
+mypath=$1
+
 echo 'Welcome to our initial pipeline !'
 echo 'First stage is starting!'
 ### Phase 1: Mutation Number Calculation & Randomsation ###
-python -u ~/project/codes/sfsdf_short.py -p '/project/Batch1' -l '/project/bedfiles/Clinical_AR_raw.bed' -it 1000 -vf strelka
+python -u $mypath/project/codes/sfsdf_short.py -p '/project/Batch1' -l '/project/bedfiles/Clinical_AR_raw.bed' -it 5 -vf strelka
 echo 'First stage is done !'
 
 echo 'Second stage is starting. TF mutation density analysis initiated'
 ### Phase x: Mutation numbers of each TF ###
 mkdir mutation_numbers_per_TF
-for i in ~/project/bedfiles/*.bed;
+for i in $mypath/project/bedfiles/*.bed;
 do
 	mybedfilename=`basename $i`
 	for b in ./final/run1/annotated_full*.vcf;
@@ -18,18 +26,18 @@ do
 	done
 done
 
-Rscript ~/project/codes/codetobuildthistable_peakOverlapWithAR.R
-Rscript ~/project/codes/COLLABORATION_Mutation_TF_dotplot.R
+Rscript $mypath/project/codes/codetobuildthistable_peakOverlapWithAR.R
+Rscript $mypath/project/codes/COLLABORATION_Mutation_TF_dotplot.R
 echo 'TF mutation density analysis finished !'
 
 echo 'Long distance mutation density analysis initiated'
 mkdir  -p long_range_density_analysis/extended_beds
 mkdir -p long_range_density_analysis/mutation_files_in_txt
-for i in ~/project/bedfiles/*.bed;
+for i in $mypath/project/bedfiles/*.bed;
 do 
 	mybedfilename=`basename $i`
 	echo "Extending $mybedfilename 5000bp from both sides"
-	bedtools slop -i $i -b 5000 -g ~/project/reference_files/hg37.chrom.sizes.txt > long_range_density_analysis/extended_beds/$mybedfilename\_5kbothsides.bed
+	bedtools slop -i $i -b 5000 -g $mypath/project/reference_files/hg37.chrom.sizes.txt > long_range_density_analysis/extended_beds/$mybedfilename\_5kbothsides.bed
 	
 	for b in ./final/run1/annotated_full*.vcf;
         do
@@ -38,7 +46,7 @@ do
 	bedtools intersect -a ./long_range_density_analysis/extended_beds/$mybedfilename\_5kbothsides.bed -b $b >long_range_density_analysis/mutation_files_in_txt/$mybedfilename\_$myvcffilename\.txt
 	done
 	
-	Rscript ~/project/codes/COLLABORATION_mutation_location_density.R long_range_density_analysis/mutation_files_in_txt/$mybedfilename\_$myvcffilename\.txt \
+	Rscript $mypath/project/codes/COLLABORATION_mutation_location_density.R long_range_density_analysis/mutation_files_in_txt/$mybedfilename\_$myvcffilename\.txt \
 	long_range_density_analysis/extended_beds/$mybedfilename\_5kbothsides.bed $mybedfilename\_$myvcffilename\_analysis long_range_density_analysis/
 done
 
